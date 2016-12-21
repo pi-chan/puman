747dbebe3a17fe8862ff84849a56941491728a65
pi-chan <xoyip@piyox.info>
2016-12-21 16:48:10 +0900

    list proxied apps

--- lib/puman/cli.rb
+++ lib/puman/cli.rb
@@ -2,9 +2,26 @@ require 'thor'
 
 module Puman
   class CLI < Thor
-    desc "list", "list all app linked with puma-dev"
-    def list
-      puts "Hello"
+    desc "proxy", "list proxyied apps linked with puma-dev"
+    def proxy
+      portmaps = {}
+      Dir.chdir File.join(Dir.home, '.puma-dev') do
+        Dir.glob('*').each do |file|
+          unless File.symlink?(file)
+            port = File.read(file)
+            portmaps[port] = file
+          end
+        end
+      end
+
+      portmaps.sort.each do |port, proj|
+        puts "#{port.strip} => #{proj}"
+      end
+    end
+
+    desc "proxify", "make a text file with new port number"
+    def proxify
+      puts 'hello'
     end
 
     desc 'version', 'version'
