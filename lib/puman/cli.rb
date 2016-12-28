744bc91a534e41d6768cee4b5006f59fb99b0dac
pi-chan <xoyip@piyox.info>
2016-12-28 22:12:49 +0900

    add an implementation file

--- lib/puman/cli.rb
+++ lib/puman/cli.rb
@@ -1,86 +1,28 @@
 require 'thor'
+require_relative 'app'
 
 module Puman
-  App = Struct.new(:name, :host_port)
-
   class CLI < Thor
     default_command :server
 
     def initialize(args, opts, config)
       super
-      correct_apps
+      @app_list = AppList.new
     end
 
     desc "list", "list all apps linked with puma-dev"
     def list
-      puts "-- symlink apps --"
-      @symlink_apps.sort_by(&:name).each do |app|
-        puts app.name
-      end
-
-      puts
-      puts "-- proxy apps --"
-      @proxy_apps.sort_by(&:host_port).each do |app|
-        puts "#{app.host_port.strip.rjust(@proxy_max_length)} => #{app.name}"
-      end
+      @app_list.list
     end
 
     desc "server", "run rails server"
     def server
-      name = File.basename(git_dir_or_current_dir)
-      apps = @proxy_apps.select{|app| app.name == name}
-      if apps.size != 1
-        puts 'no or multiple apps are defind.'
-      else
-        app = apps.first
-        exec "bundle exec rails server -p #{app.host_port}" if app.host_port.match /^\d+$/
-      end
+      @app_list.server
     end
 
     desc 'version', 'version'
     def version
       puts Puman::VERSION
-    end
-
-    # TODO
-    # desc "proxify", "make a text file with new host and port"
-    # def proxify
-    #   puts 'hello'
-    # end
-
-    private
-
-    def correct_apps
-      return if !@proxy_apps.nil? && !@symlink_apps.nil?
-      @proxy_apps = []
-      @symlink_apps = []
-      Dir.chdir File.join(Dir.home, '.puma-dev') do
-        Dir.glob('*').each do |file|
-          if File.symlink?(file)
-            @symlink_apps.push App.new(file, nil)
-          else
-            host_port = File.read(file)
-            @proxy_apps.push App.new(file, host_port)
-          end
-        end
-      end
-      @proxy_max_length = @proxy_apps.map {|app| app.host_port.strip.size}.max
-    end
-
-    def root_directory?(path)
-      File.directory?(path) && File.expand_path(path) == File.expand_path(File.join(path, '..'))
-    end
-
-    def git_dir_or_current_dir
-      current_path = Dir.pwd
-      until root_directory?(current_path)
-        if File.exist?(File.join(current_path, '.git'))
-          return current_path
-        else
-          current_path = File.dirname(current_path)
-        end
-      end
-      Dir.pwd
     end
   end
 end
--- spec/puman_spec.rb
+++ spec/puman_spec.rb
@@ -1,11 +1,26 @@
 require "spec_helper"
 
+PUMA_DEV_DIR = '~/.puma-dev'
+SAMPLE_DIR = '~/src/sample'
+
 describe Puman do
-  it "has a version number" do
+  before do
+    FakeFS.activate!
+    Dir::mkdir_p PUMA_DEV_DIR
+    Dir::mkdir_p SAMPLE_DIR
+    File.open('~/.puma-dev/proxy', 'w') { |f| f.write 'Hello, World.' }
+    File.symlink('~/.puma-dev/symlink', SAMPLE_DIR)
+  end
+
+  after do
+    FakeFS.deactivate!
+  end
+
+  it 'has a version number' do
     expect(Puman::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it '' do
     expect(false).to eq(true)
   end
 end
